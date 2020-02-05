c99fa58656b567e4a1c71c12b217b5f1e590543a
tucker <tuckerwray84@gmail.com>
2020-02-04 22:23:29 -0500

    Write out remove update scheme model helpers using knex.methods.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,30 +1,50 @@
 const db = require('../data/dbConfig')
 
+module.exports = {
+    find, findById, findSteps, add, update, remove
+}
+
 function find() {
     return db('schemes')
 }
 function findById(id) {
     return db('schemes').where({ id }).first();
 }
-function findSteps(id){
+function findSteps(id) {
     return db('schemes as m')
-    .join('steps as p','m.id','p.scheme_id')
-    .select('m.scheme_name,p.step_number,p.instructions')
-    .where({id});
+        .join('steps as p', 'm.id', 'p.scheme_id')
+        .select('m.scheme_name,p.step_number,p.instructions')
+        .where({ id });
 }
 
-async function add(newSchemeObj){
-    return new Promise((resolve, reject)=>{
-        const result= await db('schemes').insert(newSchemeObj)
-        result?
-        resolve({id:result.id,...result})
-        :
-        reject(null)
-    })
+async function add(newSchemeObj, ["scheme_name,id"]) {
+    return new Promise((resolve, reject) => {
+        const result = await db('schemes').insert(newSchemeObj)
+        result ?
+            resolve({ id: result.id, ...result })
+            :
+            reject(null)
+    });
 
 }
+// async function add(newSchemeObj) {
+//     const [id] = await db('schemes').insert(newSchemeObj)
+//     return findById(id);
+// }
 
+function update(id, changes) {
+    return db('schemes')
+        .where({ id })
+        .update({ changes });
+}
 
+function remove(id){
+    return db('scheme')
+    .where({id})
+    .first()
+    .del();
+
+}
 
 
 
@@ -36,6 +56,6 @@ async function add(newSchemeObj){
 
 async function add(hub) {
     const [id] = await db('hubs').insert(hub);
-  
+
     return findById(id);
-  }
+}
